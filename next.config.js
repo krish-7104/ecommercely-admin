e90b4ca8a9e2d0c257f69d4e61f6737294083633
Krish Jotaniya <krishjotaniya71@gmail.com>
2023-09-06 13:18:03 +0530

    Caching in api stopped

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    async headers() {
+        return [
+            {
+                source: '/api/(.*)',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'no-store, max-age=0',
+                    },
+                ],
+            },
+        ];
+    },
+};
